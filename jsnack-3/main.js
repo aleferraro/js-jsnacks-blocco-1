b5ede1fc479be2ee5ef3bf6f5ba48a1c34a8d296
Alessandro Ferraro <ale_ferraro@libero.it>
2020-11-10 17:02:30 +0100

    JSnack 3

---
+++ jsnack-3/main.js
@@ -0,0 +1,21 @@
+/*
+JSnack 3
+
+Creare un oggetto che rappresenti un triangolo rettangolo,
+con le seguenti proprietà: base e altezza.
+Calcolare perimetro e area.
+*/
+
+var triangolo = {
+  'base': 10,
+  'altezza': 8
+}
+document.write('Base: ' + triangolo.base + '<br>Altezza: ' + triangolo.altezza);
+
+var area = (triangolo.base * triangolo.altezza) / 2;
+document.write('<br>Area: ' + area);
+
+var ipotenusa = Math.sqrt((triangolo.base ** 2) + (triangolo.altezza ** 2));
+
+var perimetro = triangolo.base + triangolo.altezza + ipotenusa;
+document.write('<br>Perimetro: ' + perimetro.toFixed(2));
